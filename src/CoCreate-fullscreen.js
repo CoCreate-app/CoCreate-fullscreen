5aee5c2da69a3d801fca84832a86e28a0fdb0a05
Jean Mendoza <janco1187@gmail.com>
2020-07-05 04:07:27 +0000

    changes in readme

--- src/CoCreate-fullscreen.js
+++ src/CoCreate-fullscreen.js
@@ -37,21 +37,32 @@ var requestFullscreen = function (ele) {
     }
   };
   var FullscreenItem = function (item) {
-    item = event.target;
-    var target = item.getAttribute("target") == null ? document.documentElement : document.getElementById(item.getAttribute("target"))
+    //item = event.target;
+    var target = item.getAttribute("target") == null ? document.documentElement : document.querySelector(item.getAttribute("target"))
+    //console.log('target => ',target)
     requestFullscreen(target);
   }
   
   var list_fs_target_button = document.getElementsByClassName("fs-toggle")
   for (let item of list_fs_target_button) {
       item.addEventListener("click", function(event) {
+        //console.log(item)
         FullscreenItem(item)
       });
       item.addEventListener("click", function(event) {
           exitFullscreen(item);
         });
   }
+  //var list_fs_exit_button = document.getElementsByClassName("fs-exit-page-button")
+  var list_fs_exit_button = document.querySelectorAll(".fs-exit-page-button")
+  //console.log(list_fs_exit_button)
+  list_fs_exit_button.forEach(item=>{
+    item.addEventListener("click", function(event) {
+      exitFullscreen(item);
+    });
+  })
 
+/*
 document.addEventListener("fullscreenchange", function () {
     console.log('-')
 }, false);
@@ -67,3 +78,4 @@ document.addEventListener("webkitfullscreenchange", function () {
 document.addEventListener("msfullscreenchange", function () {
     console.log('-')
 }, false);
+*/
