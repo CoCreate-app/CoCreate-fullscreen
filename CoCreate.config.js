443d2e9ca4b6c84decbd7861433ec0f92dcc49b0
frankpagan <frank@cocreate.app>
2021-02-07 02:02:53 +0000

    test doc

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "601f49be6b402c72efc07e09",
             key: "html",
             data:{
                 name: "Fullscreen Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "601f49be6b402c72efc07e08",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "601f49be6b402c72efc07e09",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/fullscreen",
